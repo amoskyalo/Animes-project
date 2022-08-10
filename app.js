f5b7a64d8dbffdd4a21254ee59908306780c8c0e
amoskyalo <amoskyalo927@gmail.com>
2022-08-11 01:03:43 +0300

    minor changes done

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-console.log("Hello");
-
 const toggler = document.querySelector('.toggler');
 const navMenu = document.querySelector('.nav-links');
 
 toggler.addEventListener('click', ()=>{
     navMenu.classList.toggle("active");
 });
+
+const navLinks = document.querySelectorAll('.nav-link');
+console.log(navLinks);
+
+for(const navLink of navLinks){
+    navLink.addEventListener('click', ()=>{
+        navMenu.classList.remove("active");
+    });
+}
