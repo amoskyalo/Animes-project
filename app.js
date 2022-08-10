f79946044505072deb7b28d9f49e09a04d450806
amoskyalo <amoskyalo927@gmail.com>
2022-08-11 02:56:27 +0300

    implemented sorting

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//nav-bar:
 const toggler = document.querySelector('.toggler');
 const navMenu = document.querySelector('.nav-links');
 
@@ -6,10 +7,63 @@ toggler.addEventListener('click', ()=>{
 });
 
 const navLinks = document.querySelectorAll('.nav-link');
-console.log(navLinks);
-
 for(const navLink of navLinks){
     navLink.addEventListener('click', ()=>{
         navMenu.classList.remove("active");
     });
 }
+
+
+//sorting using sort() method:
+const names = [
+    {
+        "name": "Kimilu"
+    },
+    {
+        "name": "Amos"
+    },
+    {
+        "name": "Samuel"
+    },
+    {
+        "name": "Faith"
+    },
+    {
+        "name": "Kyalo"
+    },{
+        "name": "Angeline"
+    }
+];
+
+const myOutput = document.querySelector('.contents');
+
+var output = "";
+for(let personName of names){
+    output+=`
+        <p class="name">${personName["name"]}</p>`;
+};
+
+myOutput.innerHTML = output;
+
+var sorted = names.sort((a, b)=>{
+    if(a.name < b.name){
+        return -1;
+    }
+    if(a.name > b.name){
+        return 1;
+    }
+    return 0;
+});
+
+var output2 = "";
+for(sortedName of names){
+    output2+=`
+    <p class="name">${sortedName["name"]}</p>`
+};
+console.log(output2);
+
+const sortButton = document.querySelector('.sort');
+sortButton.addEventListener("click", ()=>{
+    myOutput.innerHTML = output2;
+});
+
