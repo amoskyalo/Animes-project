bca5c5526e0da91fd44144ba5fdc4106c56ed82b
amoskyalo <amoskyalo927@gmail.com>
2022-08-11 03:59:00 +0300

    added the watch button

--- app.js
+++ app.js
@@ -18,26 +18,32 @@ for(const navLink of navLinks){
 const names = [
     {
         "name": "Blood of zeus",
-        "image": "zeus.png"
+        "image": "zeus.png",
+        "link": "https://www.netflix.com/ke/title/81001988?source=35"
     },
     {
         "name": "Naruto",
-        "image": "naruto.jpg"
+        "image": "naruto.jpg",
+        "link": "https://www.youtube.com/watch?v=Y9P1EhndE0w"
     },
     {
         "name": "Demon slayer",
-        "image": "slayer.jpeg"
+        "image": "slayer.jpeg",
+        "link": "https://www.imdb.com/video/vi2476588825/?ref_=tt_vi_i_1"
     },
     {
         "name": "One piece",
-        "image": "one-piece.jpeg"
+        "image": "one-piece.jpeg",
+        "link": "https://www.youtube.com/watch?v=-3g8Nnehdao"
     },
     {
         "name": "Anime girl",
-        "image": "anime-girl.jpg"
+        "image": "anime-girl.jpg",
+        "link": "https://www.bilibili.tv/en/video/2049239198"
     },{
         "name": "Yasuke",
-        "image": "yasuke.jpeg"
+        "image": "yasuke.jpeg",
+        "link": "https://www.netflix.com/ke/title/80990863"
     }
 ];
 
@@ -49,6 +55,7 @@ for(let personName of names){
     <div class="animes">
         <img src="${personName["image"]}"/>
         <p class="name">${personName["name"]}</p>
+        <a class="watch" href="${personName.link}">Watch</a>
     </div>`;
 };
 
@@ -70,9 +77,9 @@ for(sortedName of names){
     <div class="animes">
         <img src="${sortedName["image"]}"/>
         <p class="name">${sortedName["name"]}</p>
+        <a class="watch" href="${sortedName["link"]}"></a>
     </div>`;
 };
-console.log(output2);
 
 const sortButton = document.querySelector('.sort');
 sortButton.addEventListener("click", ()=>{
